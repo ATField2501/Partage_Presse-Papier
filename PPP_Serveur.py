13ad9c899f6a67e421a834d8cee8f7a35784dc98
Cagliostro <atfield2501@gmail.com>
2019-04-26 14:14:45 +0200

    /Inversement du comportement. standart == crypté. Plus cohérent

---
+++ PPP_Serveur.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# -*- coding: utf8 
+
+import socket
+from cryptography.fernet import Fernet
+
+
+#### Appelé sans argument, le serveur n'accepte que les datas cryptés
+#### Si un argument lui est passé (*) , il n'accepte que les datas claires
+
+hote = ''
+port = 12800
+aleph = False
+
+if len(sys.argv) == 2:
+    aleph = True
+
+## Initialisation
+connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_principale.bind((hote, port))
+connexion_principale.listen(5)
+
+
+print("Presse Papier Partagé à l'écoute sur le port {}".format(port))
+
+
+## Crypté
+if aleph == False:
+    key = input("Entrez la clée de décryptage: \n")
+    cipher_suite = Fernet(key)
+
+    connexion_avec_client, infos_connexion = connexion_principale.accept()
+
+    while 1:
+        msg_recu = connexion_avec_client.recv(1024)
+        message_claire = cipher_suite.decrypt(msg_recu)
+        print(message_claire.decode())
+    print("Fermeture de la connexion")
+
+connexion_avec_client.close()
+#connexion_principale.close()
+
+
+## Claire
+else:
+    connexion_avec_client, infos_connexion = connexion_principale.accept()
+
+    while 1:
+        msg_recu = connexion_avec_client.recv(1024)
+        message_claire = cipher_suite.decrypt(msg_recu)
+        print(message_claire.decode())
+    print("Fermeture de la connexion")
+
+connexion_avec_client.close()
--- PartagePressePapier.py
+++ PartagePressePapier.py
@@ -6,10 +6,20 @@
 import os
 import socket
 import time
+import sys
 
 
 from config import *
 from CaglioPPPmodule import *
+
+aleph = False
+
+# si argument passer à la commande on enclenche le mode crypté
+# Lecture des arguments
+if len(sys.argv) == 2:
+    aleph = True
+
+
 
 #Création du client
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,13 +36,31 @@ try:
 except Exception as e:
     print(str(e))
 
-#On génère un clée de cryptage
-crypt=Hocuspocus()
 
-while 1:
-    obj=Nazghul(connexion_avec_serveur)
-    msg_recu = connexion_avec_serveur.recv(1024)
+## Claire
+if aleph == True:
+    while 1:
+        presse_papier= os.popen('xclip -o 2>/dev/null' ).readlines()
+        presse_papier2=""
+        for e in presse_papier:
+            presse_papier2 += e
+        if len(presse_papier2) != 0:
+            b = bytes(presse_papier2, 'utf-8')
+            # On envoie le message
+            connexion_avec_serveur.send(b)
+        time.sleep(0.6) 
+
+    connexion_avec_serveur.close()
+
+## Crypté
+else:
+    #On génère un clée de cryptage
+    crypt=Hocuspocus()
+    while 1:
+        obj=Nazghul(connexion_avec_serveur)
+        msg_recu = connexion_avec_serveur.recv(1024)
 #    msg_recu = connexion_avec_client.recv(1024)
-    time.sleep(0.6) 
+        time.sleep(0.6) 
+    connexion_avec_serveur.close()
 
-connexion_avec_serveur.close()
+
